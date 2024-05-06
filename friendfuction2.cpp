d5f689d054d3930752cfed25d9685e56bab5f7dd
rnktlhr <rinakitluhur10@gmail.com>
2024-05-06 15:33:01 +0700

    menambahkan class pelajar dan manusia

--- friendfuction2.cpp
+++ friendfuction2.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class pelajar;
+class manusia {
+    public:
+    void showNilaiPelajar(pelajar&x);
+};
+
+class pelajar{
+    private:
+    int nilai;
+    public:
+    pelajar(){nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar&x);
+};
+
 
 int main(){
-    
+
 }
