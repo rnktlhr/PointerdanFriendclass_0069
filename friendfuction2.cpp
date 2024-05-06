6fe2fd9b4f09501d5157f4b05d793aebc395a494
rnktlhr <rinakitluhur10@gmail.com>
2024-05-06 15:37:54 +0700

    menambahkan int main

--- friendfuction2.cpp
+++ friendfuction2.cpp
@@ -16,9 +16,12 @@ class pelajar{
 };
 
 void manusia::showNilaiPelajar(pelajar&x){
-    std::cout << x.nilai;
+    cout << x.nilai;
 }
 
 int main(){
-
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
 }
